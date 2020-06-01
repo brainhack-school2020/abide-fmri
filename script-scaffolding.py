03c2ea1d3fac31f57f14bfed3cb9cca9e31f60d7
mschoettner <mikkel.schoettner@yahoo.de>
2020-06-01 19:09:39 +0200

    Fixed indentation error

--- script-scaffolding.py
+++ script-scaffolding.py
@@ -37,7 +37,7 @@ def prepare_data(data_dir, output_dir, pipeline = "cpac", quality_checked = True
         feat_file = os.path.join(output_dir, 'ABIDE_BASC064_features.npz')
         X_features = np.load(feat_file)['a']
         print("Feature file found.")
-        
+
     except: # if not, extract features
         X_features = [] # here is where we will put the data (a container)
         print("No feature file found. Extracting features...")
@@ -50,30 +50,30 @@ def prepare_data(data_dir, output_dir, pipeline = "cpac", quality_checked = True
             X_features.append(correlation_matrix)
             # keep track of status
             print('finished extracting %s of %s'%(i+1,len(fmri_filenames)))
-            
-     # Extract half of the matrix for each subject -> (871, 4096) to (871, 2080)
-     for i in range (len(X_features)):
-            X_features[i]=list(X_features[i][np.triu_indices(64)])
-     i=i+1
-        
-     # Save features
-     np.savez_compressed(os.path.join(output_dir, 'ABIDE_BASC064_features'),
+
+    # Extract half of the matrix for each subject -> (871, 4096) to (871, 2080)
+    for i in range(len(X_features)):
+        X_features[i]=list(X_features[i][np.triu_indices(64)])
+    i=i+1
+
+    # Save features
+    np.savez_compressed(os.path.join(output_dir, 'ABIDE_BASC064_features'),
                             a = X_features)
-    
-    # Dimensionality reduction of features with PCA 
+
+    # Dimensionality reduction of features with PCA
     pca = PCA(0.99).fit(X_features) # keeping 99% of variance
     X_features_pca = pca.transform(X_features)
-    
+
     #print("original shape: ", X_features.shape)
     #print("transformed shape:", X_features_pca.shape)
 
-    
+
     # Transform phenotypic data into dataframe
     abide_pheno=pd.DataFrame(abide.phenotypic)
-    
+
     # Get the target vector
     y_target= abide_pheno['DX_GROUP']
-    
+
 
 def run_analysis():
     description = "Train classifier on the ABIDE data to predict autism"
