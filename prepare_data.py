72d8bf44b6200f1d2eaf51095ef28e15b77a7d5e
mschoettner <mikkel.schoettner@yahoo.de>
2020-06-03 16:51:27 +0200

    Fixed bugs in prepare_data.py
    Fixed bugs where the wrong environment was referenced and the wrong variable name returned.

--- prepare_data.py
+++ prepare_data.py
@@ -1,4 +1,4 @@
-#!/Users/emilychen/miniconda3 python
+#!/usr/bin/env python
 
 from nilearn import datasets
 from nilearn.input_data import NiftiLabelsMasker
@@ -83,8 +83,8 @@ def run():
                         outputs.""")
     args = parser.parse_args()
     X_features_pca, y_target = prepare_data(args.data_dir, args.output_dir)
-    
-    return(X_features, y_target)
+
+    return(X_features_pca, y_target)
 
 
 if __name__ == "__main__":
